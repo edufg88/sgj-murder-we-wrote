e6bf0ab116bebdcd79b3fb7beff4fb86e6fda95a
gonzalezfranks@hotmail.com <gonzalezfranks@hotmail.com@0129d1ee-40f0-529c-3618-8e47186deec4>
2015-01-24 23:53:58 +0000

    Añadido el control de luces

---
+++ trunk/Assets/Scripts/ControlLuces.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlLuces : MonoBehaviour {
+
+	public Animation animationLightRoomOne;
+	public Animation animationLightRoomTwo;
+	public Animation animationLightRoomThree;
+	
+
+	#region Patron Singleton
+	private static ControlLuces _instancia = null;
+	public static ControlLuces Instancia{
+		get
+		{
+			if(_instancia == null)
+			{
+				_instancia = FindObjectOfType<ControlLuces>();
+			}
+			return _instancia;
+		}
+	}
+	#endregion
+
+	void Start(){
+	
+		animationLightRoomOne = GetChildByName("LuzOne", this.transform).GetComponent<Animation>();
+
+		animationLightRoomTwo = GetChildByName("LuzTwo", this.transform).GetComponent<Animation>();
+
+		animationLightRoomThree = GetChildByName("LuzThree", this.transform).GetComponent<Animation>();
+	}
+
+	public void TurnOnRoomOne(){
+		animationLightRoomOne.Play("Luz");
+	}
+
+	public void TurnOnRoomTwo(){
+		animationLightRoomTwo.Play("Luz");
+	}
+
+	public void TurnOnRoomThree(){
+		animationLightRoomThree.Play("Luz");
+	}
+
+
+	private Transform GetChildByName(string name, Transform t){
+
+		for(int i = 0; i < t.childCount; i++){
+
+			if(t.GetChild(i).name == name){
+				return t.GetChild(i);
+			}
+
+		}
+
+		return null;
+	}
+
+}
