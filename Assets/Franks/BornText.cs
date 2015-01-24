47ab78f9f73610f553b807a5c1a510c3e5b4286e
gonzalezfranks@hotmail.com <gonzalezfranks@hotmail.com@0129d1ee-40f0-529c-3618-8e47186deec4>
2015-01-24 14:52:55 +0000

    +GameGUI.cs interfaz

---
+++ Assets/Franks/BornText.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BornText : MonoBehaviour {
+
+	int prueba = 20;
+	// Use this for initialization
+	void Start () {
+	
+		NotificationCenter.DefaultCenter().AddObserver(this, "Bocadillo");
+
+	}
+
+	void Bocadillo(Notification notification){
+		Debug.Log("Me llamas");
+		TextoFlotante.Show(string.Format("+{0}!", prueba), "PointStarText", new FromWorldPointTextPositioner(Camera.main, this.transform.position, 1.5f, 50));
+	}
+
+}
