43e3bba5d472ce2a80ba84efe87599e218593268
edufg88@gmail.com <edufg88@gmail.com@0129d1ee-40f0-529c-3618-8e47186deec4>
2015-01-25 05:00:49 +0000

    + Finales are decided according to points

--- Assets/Scripts/Texts.cs
+++ Assets/Scripts/Texts.cs
@@ -60,19 +60,19 @@ public static class Texts {
 
 	//Finales
 
-	public static string derositas1 = "The neigborhood has heard strange noises in the house but everything seems fine. Good night!";
-	public static string lospillan2 = "Put yours arms in the ground, now! You are under arrest";
-	public static string ebola3 = "Due to poor hygiene in the house, you have Ebola infected by a bat.";
-	public static string plaga4 = "A FEMAs major operation unfolds around the house.";
-	public static string incendio5 = "OOOHHH, There are fire everywhere. Its the final. God, help me, help me!";
-	public static string inundacion6 = "Yeeeeh, you dropped a bowl of water? OH, NO! It has burst a pipe and the door can not be opened. Do you have a floater?";
-	public static string nicolas7 = "Francisco Nicolas has arrived ti the house and you take a selfie with him. It has been a great day.";
-	public static string secta8 = "Because of your bad behavior, you decide to go away to a sect that makes orgies to prepare the coming of the extraterretres.";
-	public static string suicidio9 = "We will not get out of this it is best that we take what is left is acid ... glub, glub ... aaaarggghhh ...";
-	public static string moldeAnoBombon10 = "A pastry appears and says, takes the position that I'll make a chocolate mold in your anus. It is fashionable.";
-	public static string comida11 = "For what remains free, do you want to eat a turnip?";
-	public static string WDWDN12 = "Everithing is ok....What do we do now?";
-	public static string premioVecindario13 = "Your house is so clean that the neighborhood has given you an award for most beautiful.";
-	public static string culpableA_14 = "The police was in homicides and has detected that the murderer is Player A.";
-	public static string culpableB_15 = "The police was in homicides and has detected that the murderer is Player B.";
+	public static string final_1_derositas = "The neigborhood has heard strange noises in the house but everything seems fine. Good night!";
+	public static string final_2_lospillan = "Put yours arms in the ground, now! You are under arrest";
+	public static string final_3_ebola = "Due to poor hygiene in the house, you have Ebola infected by a bat.";
+	public static string final_4_plaga = "A FEMAs major operation unfolds around the house.";
+	public static string final_5_incendio = "OOOHHH, There are fire everywhere. Its the final. God, help me, help me!";
+	public static string final_6_inundacion = "Yeeeeh, you dropped a bowl of water? OH, NO! It has burst a pipe and the door can not be opened. Do you have a floater?";
+	public static string final_7_nicolas = "Francisco Nicolas has arrived ti the house and you take a selfie with him. It has been a great day.";
+	public static string final_8_secta = "Because of your bad behavior, you decide to go away to a sect that makes orgies to prepare the coming of the extraterretres.";
+	public static string final_9_suicidio = "We will not get out of this it is best that we take what is left is acid ... glub, glub ... aaaarggghhh ...";
+	public static string final_10_moldeAnoBombon = "A pastry appears and says, takes the position that I'll make a chocolate mold in your anus. It is fashionable.";
+	public static string final_11_comida = "For what remains free, do you want to eat a turnip?";
+	public static string final_12_WDWDN = "Everithing is ok....What do we do now?";
+	public static string final_13_premioVecindario = "Your house is so clean that the neighborhood has given you an award for most beautiful.";
+	public static string final_14_culpableA = "The police was in homicides and has detected that the murderer is Player A.";
+	public static string final_15_culpableB = "The police was in homicides and has detected that the murderer is Player B.";
 }
